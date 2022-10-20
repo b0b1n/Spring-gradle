b6d007b3a260d3a9475fe3f4b2af7a1636256b33
b0b1n <sohaibskious@gmail.com>
2022-10-20 16:21:55 +0200

    Multi-module project configured

--- src/main/java/SpringGr/gradledemo/GradleDemoApplication.java
+++ src/main/java/SpringGr/gradledemo/GradleDemoApplication.java
@@ -3,8 +3,11 @@ package SpringGr.gradledemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.websocket.server.ServerEndpoint;
+
 @SpringBootApplication
 public class GradleDemoApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GradleDemoApplication.class, args);
